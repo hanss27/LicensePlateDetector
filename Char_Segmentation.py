fe9dd3e82dc4801acebafca36a0adeaa62407fa0
hanss27 <raihan.muhammad91@ui.ac.id>
2022-06-03 21:56:17 +0700

    Fix variables name

--- Char_Segmentation.py
+++ Char_Segmentation.py
@@ -1,22 +1,25 @@
 import cv2
 import imutils
 import numpy as np
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.image import img_to_array
+
 import functools
 from pathlib import Path
 import os
 
-def connected(image, showSteps=False):
-        image = cv2.imread(image)
-        image = imutils.resize(image, height=250, width=400 )
+def connected(imag, img_name,showSteps=False):
+        image = cv2.imread(imag)
+        image = cv2.resize(image,(300,120) )
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         gray = 255-gray
         gray = cv2.bilateralFilter(gray, 11, 17, 17) 
         blurred = cv2.GaussianBlur(gray, (5, 5), 0)
         thresh = cv2.adaptiveThreshold(blurred, 255,
-        cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 45, 15)
-        
+        cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 43, 9)
+        thresh = cv2.erode(thresh, (3,3))
+        thresh = cv2.dilate(thresh, (3,3))
+        thresh = cv2.dilate(thresh, (3,3))
+        thresh = cv2.erode(thresh, (3,3))
+
         _, labels = cv2.connectedComponents(thresh)
 
 
@@ -24,8 +27,8 @@ def connected(image, showSteps=False):
 
         # Set lower bound and upper bound criteria for characters
         total_pixels = image.shape[0] * image.shape[1]
-        lower = total_pixels // 150 # heuristic param, can be fine tuned if necessary
-        upper = total_pixels // 10 # heuristic param, can be fine tuned if necessary
+        lower = total_pixels // 200 # heuristic param, can be fine tuned if necessary
+        upper = total_pixels // 5 # heuristic param, can be fine tuned if necessary
         # Loop over the unique components
         for (i, label) in enumerate(np.unique(labels)):
                 # If this is the background label, ignore it
@@ -61,83 +64,48 @@ def connected(image, showSteps=False):
                         return rect1[0] - rect2[0]
 
         boundingBoxes = sorted(boundingBoxes, key=functools.cmp_to_key(compare))
-        charpic = []
+        detected_char_list = []
         new_mask = mask.copy()
         for c in boundingBoxes:
                 x,y,w,h = c
                 x = x-2
-                if y-2 >=0:
-                        y = y-2
+                y = y-2
                 w = w+2
                 h = h+2
-                if w>5 and h>10: 
-                        cv2.rectangle(new_mask,(x,y),(x+w,y+h),(255,255,0),3)
-                        charpic.append(mask[y:y+h, x:x+w])
+                if w>5 and h>10 and x > 10 and x < 280 and y > 10 and y < 60 : 
+                        cv2.rectangle(new_mask,(x,y),(x+w,y+h),(0,0,0),3)
+                        detected_char_list.append(mask[y:y+h, x:x+w])
         cv2.imshow("detected_char",new_mask)
-        crop(charpic)
+        output(detected_char_list, img_name)
         return mask
 
-def crop(charpic):
+def output(detected_char_list, img_name):
         i = 1
         path = os.path.join(Path().absolute(), "detected_chars") # Directory of current working directory, not __file__ 
+        path = os.path.join(path, img_name[:-3])
         if not os.path.exists(path):
                 os.makedirs(path)
-        for pic in charpic:                
+
+        for pic in detected_char_list:  
+                cv2.resize(pic, (70,100))
+                print(pic)
                 pic_format = "test"+str(i)+".jpg" 
                 cv2.imwrite(os.path.join(path, pic_format), pic)
                 i+=1
-def detect_plate(imag, showSteps = False):
-        image = cv2.imread(imag)
-       
-        image = imutils.resize(image, width=300)
-
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        gray = 255-gray
-        gray = cv2.GaussianBlur(gray,(5,5),0)
-        gray = cv2.bilateralFilter(gray, 11, 17, 17) 
-
-        edged = cv2.Canny(gray, 30, 200) 
-        cnts,new = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-        image1=image.copy()
-        cv2.drawContours(image1,cnts,-1,(0,255,0),3)
-
-        cnts = sorted(cnts, key = cv2.contourArea, reverse = True) [:60]
-        screenCnt = None
-        image2 = image.copy()
-        cv2.drawContours(image2,cnts,-1,(0,255,0),3)
-
-        i=1
-        for c in cnts:
-                perimeter = cv2.arcLength(c, True)
-                approx = cv2.approxPolyDP(c, 0.018 * perimeter, True)
-                if len(approx) == 4: 
-                        screenCnt = approx
-                        x,y,w,h = cv2.boundingRect(c) 
-                        new_img=image[y:y+h,x:x+w]
-                        cv2.imwrite('./'+str(i)+'.jpg',new_img)
-                        i+=1
-                        break
-        if showSteps == True:
-                cv2.imshow("Original Image", image)
-                cv2.imshow("B&W Image", gray)
-                cv2.imshow("Canny Image", edged)
-                cv2.imshow("Contours",image1)
-                cv2.imshow("Top 30 contours",image2)
-
-        if screenCnt is not None:
-                cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 3)
-                cv2.imshow("image with detected license plate", image)
-                return i-1
-        else:
-                print("Image not detected")
-                return -1
 
 def main():
-        path = os.path.join(Path().absolute(), "src_img") # Directory of current working directory, not __file__ 
-        a = detect_plate(os.path.join(path,"test_img.jpg"), showSteps=True)
-        if a is not -1:
-                a = str(a) + ".jpg"
-                mask = connected(a, showSteps=False)
+        folder_path = "src_img" # Change the path with the folder with imgs
+        path = os.path.join(Path().absolute(),folder_path) # Directory of current working directory, not __file__ 
+        img = "plat2.jpg" # Change it for a single testing
+        mask = connected(os.path.join(path, img), img, showSteps=True)
+       
+        # Use this code below for loop all the imgs inside a folder
+        # path_list = os.listdir(path)
+        # for img in path_list:
+        #         try:
+        #                  mask = connected(os.path.join(path,img), img, showSteps=False)
+        #         except:
+        #                 continue
 
         cv2.waitKey(0)
 if __name__=="__main__":
